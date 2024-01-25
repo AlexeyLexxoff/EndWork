3be024bd7eb862ecfb7e3425841c160ba429be82
AlexeyLexxoff <djlexxoff@gmail.com>
2024-01-25 18:09:59 +0300

    add main class

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] array1 = new string[5] { "321", "12", "geekbrains", "computer", "bom" };
+        string[] array2 = new string[array1.Length];
+        void SecondArray(string[] array1, string[] array2)
+        {
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
+        }
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+        SecondArray(array1, array2);
+        PrintArray(array2);
+    }
+}
